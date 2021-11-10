7a7651d3fbcbff86c4da835c374d479fb52affd4
Vendra <vendra0408@gmail.com>
2021-11-10 13:10:39 +0530

    Initial Commit

---
+++ src/StrategyDesignPattern/Controller/ScoreBoard.java
@@ -0,0 +1,9 @@
+package StrategyDesignPattern.Controller;
+
+public class ScoreBoard {
+    public ScoreAlgorithmBase algorithmBase;
+
+    public void showScore(int taps, int multiplier) {
+        System.out.println(algorithmBase.calculateScore(taps, multiplier));
+    }
+}
