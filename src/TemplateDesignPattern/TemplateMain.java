e2681925c1f3e8d6ee2749dd7d91febd46f6dcc0
Vendra <vendra0408@gmail.com>
2021-11-11 21:13:50 +0530

    Initial Commit

---
+++ src/TemplateDesignPattern/TemplateMain.java
@@ -0,0 +1,12 @@
+package TemplateDesignPattern;
+
+public class TemplateMain {
+
+    public static void main(String[] args) {
+
+        Game game = new EndlessRunnerGame();
+        game.play();
+
+
+    }
+}
