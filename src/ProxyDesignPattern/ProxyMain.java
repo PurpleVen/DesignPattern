811ab96ce710cead7ecd890dd92d6d1458b5f4c5
Vendra <vendra0408@gmail.com>
2021-11-12 20:31:30 +0530

    Initial Commit

---
+++ src/ProxyDesignPattern/ProxyMain.java
@@ -0,0 +1,15 @@
+package ProxyDesignPattern;
+
+public class ProxyMain {
+
+    public static void main(String[] args) {
+        Bank bank = new ProxyBank();
+
+        try {
+            bank.withdrawMoney("Paulo");
+            bank.withdrawMoney("me@me");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
